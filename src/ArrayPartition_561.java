85cf8b11058f4f627f6c84d8c7d0faed8c980411
Gogs <gogs@fake.local>
2021-02-16 22:58:19 +0800

    finish 561 ArrayPartition

---
+++ src/ArrayPartition_561.java
@@ -0,0 +1,50 @@
+import java.util.Collections;
+
+public class ArrayPartition_561 {
+/*
+给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+返回该 最大总和 。
+ */
+public  int arrayPairSum(int[] nums) {
+    int sum=0;
+    int n=nums.length;
+
+   quick_sort(nums,0,n);
+
+    for(int flag=0;flag<n;flag+=2)
+    {
+        sum+=nums[flag];
+    }
+
+    return sum;
+}
+
+    void quick_sort(int s[], int l, int r)
+    {
+        if (l < r)
+        {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j)
+            {
+                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if(i < j)
+                    s[i++] = s[j];
+
+                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if(i < j)
+                    s[j--] = s[i];
+            }
+            s[i] = x;
+            quick_sort(s, l, i - 1); // 递归调用
+            quick_sort(s, i + 1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
